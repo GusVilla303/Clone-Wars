38c20c838715b034b0048b3d42a26e80f4857130
Chad Brading <chadbrading@gmx.com>
2014-07-31 02:42:31 -0600

    cms works for specialty sandwiches

--- lib/app.rb
+++ lib/app.rb
@@ -116,11 +116,11 @@ class BackCountry < Sinatra::Base
   end
 
   get '/specialty_sandwiches' do
-    erb :menu_specialty_sandwiches
+    erb :menu_specialty_sandwiches, locals: {menu: pages.connection[:menus].to_a[1], items: pages.connection[:items].where(:menu_id => '2').to_a, menu_headings: pages.connection[:menu_headings].where(:menu_id =>  '2').to_a}
   end
 
   get '/hot_breakfast_sandwiches' do
-    erb :menu_hot_breakfast_sandwiches
+    erb :menu_hot_breakfast_sandwiches, locals: {menu: pages.connection[:menus].to_a[0], items: pages.connection[:items].where(:menu_id => '1').to_a, menu_headings: pages.connection[:menu_headings].where(:menu_id =>  '1').to_a}
   end
 
   get '/build_your_own_sandwiches' do
@@ -136,7 +136,11 @@ class BackCountry < Sinatra::Base
   end
 
   get '/admin_hot_breakfast_sandwiches' do
-    erb :admin_menu_hot_breakfast_sandwiches, locals: {menu: pages.connection[:menus].to_a[0], items: pages.connection[:items].where(:menu_id => 1).to_a, menu_headings: pages.connection[:menu_headings].where(:menu_id =>  1).to_a}
+    erb :admin_menu_hot_breakfast_sandwiches, locals: {menu: pages.connection[:menus].to_a[0], items: pages.connection[:items].where(:menu_id => '1').to_a, menu_headings: pages.connection[:menu_headings].where(:menu_id =>  '1').to_a}
+  end
+
+  get '/admin_specialty_sandwiches' do
+    erb :admin_menu_specialty_sandwiches, locals: {menu: pages.connection[:menus].to_a[1], items: pages.connection[:items].where(:menu_id => '2').to_a, menu_headings: pages.connection[:menu_headings].where(:menu_id =>  '2').to_a}
   end
 
   post '/admin_menu_item/:attribute/:item_id/:menu_id' do |attribute, item_id, menu_id|
